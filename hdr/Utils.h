7da71bc67d78cbe7693608d183a2eb23733e381f
Chris Allen <58678379+Skelt3r@users.noreply.github.com>
2022-09-09 23:32:28 -0500

    Change array to vector and add input parser

--- hdr/Utils.h
+++ hdr/Utils.h
@@ -1,3 +1,4 @@
 #pragma once
-void cleanup(int rolls[], std::string &result, char &oper, int &mod, int &sum);
+void cleanup(std::vector<int> &rolls, std::string &result, char &oper, int &mod, int &sum);
 void error_message(int id);
+bool parse_input(char &d, char &oper, int &num_dice, int &num_sides);
